7f5607d66280b572f19b3865e617be5fbd4639bc
Hakim <akoredeagn@gmail.com>
2025-02-08 12:18:26 +0100

    Update config.php
    Essential information to allow the application to send message to an email.

--- config.php
+++ config.php
@@ -1,7 +1,5 @@
 <?php 
     define("SMTP_USER", "akimakoredeagnide@gmail.com");
-    // define("SMTP_PASS", "zijl dsyb qxyp lklk");
-    define("SMTP_PASS", "bytt dikm swjt heyo");
-
-    
+    // define("SMTP_PASS", "zijl dsyb qxyp lklk"); uncomment this lign and complete the variable STMP_PASS with your own right
+                                                // value. I mean the application pass to access/allow sending message to your email.
 ?>
