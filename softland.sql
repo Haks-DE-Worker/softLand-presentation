d0a02c2a138c333bef8a2b23503a5be9973bb5a8
Hakim <akoredeagn@gmail.com>
2025-04-10 16:05:58 +0200

    bd-soft uploading

---
+++ softland.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : jeu. 10 avr. 2025 à 16:03
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `softland`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `articles`
+--
+-- Erreur de lecture de structure pour la table softland.articles : #1932 - Table &#039;softland.articles&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table softland.articles : #1064 - Erreur de syntaxe près de &#039;FROM `softland`.`articles`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fonctionnaliteproduits`
+--
+-- Erreur de lecture de structure pour la table softland.fonctionnaliteproduits : #1932 - Table &#039;softland.fonctionnaliteproduits&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table softland.fonctionnaliteproduits : #1064 - Erreur de syntaxe près de &#039;FROM `softland`.`fonctionnaliteproduits`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fonctionnalites`
+--
+-- Erreur de lecture de structure pour la table softland.fonctionnalites : #1932 - Table &#039;softland.fonctionnalites&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table softland.fonctionnalites : #1064 - Erreur de syntaxe près de &#039;FROM `softland`.`fonctionnalites`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produits`
+--
+-- Erreur de lecture de structure pour la table softland.produits : #1932 - Table &#039;softland.produits&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table softland.produits : #1064 - Erreur de syntaxe près de &#039;FROM `softland`.`produits`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `profiles`
+--
+-- Erreur de lecture de structure pour la table softland.profiles : #1932 - Table &#039;softland.profiles&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table softland.profiles : #1064 - Erreur de syntaxe près de &#039;FROM `softland`.`profiles`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+-- Erreur de lecture de structure pour la table softland.users : #1932 - Table &#039;softland.users&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table softland.users : #1064 - Erreur de syntaxe près de &#039;FROM `softland`.`users`&#039; à la ligne 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
